d6e99a8edfdaf1df3125dcdb47c979923b208b25
Percy Castro <percycastrogomez@gmail.com>
2026-05-07 18:56:13 -0500

    implement csvreader

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include "../include/Utils.h"
+#include "../include/CsvReader.h"
+#include "../include/Movie.h"
+
 using namespace std; 
 
 int main() {
-	cout << "Trieflix initialized\n"; 
-	return 0; 
+
+	CsvReader reader;
+
+    	std::vector<Movie> movies = reader.readMovies("../data/raw/movies.csv");
+
+	std::cout << "Movies loaded: "
+		<< movies.size()
+		<< std::endl;
+
+    	for (int i = 0; i < 5 && i < movies.size(); i++) {
+
+        std::cout << "\nTITLE: "
+                  << movies[i].title
+                  << std::endl;
+
+        std::cout << "\nDIRECTOR: "
+                  << movies[i].director
+                  << std::endl;
+
+        std::cout << "PLOT: "
+                << movies[i].plot
+                << std::endl;
+    	}
+
+    	return 0;
 }
